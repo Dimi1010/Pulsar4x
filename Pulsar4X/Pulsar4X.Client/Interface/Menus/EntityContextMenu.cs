538f11d0be909ed149fa508a50b7fe304f7c6480
Dimitar Krastev <dimi1010100@gmail.com>
2026-08-22 11:31:22 +0300

    Port the rest of the actions.

--- Pulsar4X/Pulsar4X.Client/Interface/Menus/EntityContextMenu.cs
+++ Pulsar4X/Pulsar4X.Client/Interface/Menus/EntityContextMenu.cs
@@ -6,12 +6,29 @@ using System.Collections.Generic;
 
 namespace Pulsar4X.Client
 {
+    /// <summary>
+    /// A base class for entity actions that are displayed in <see cref="EntityContextMenu"/>.
+    /// </summary>
     public abstract class EntityContextAction
     {
         public abstract string Name { get; }
 
+        /// <summary>
+        /// Checks if the action can be performed for the selected entity.
+        /// </summary>
+        /// <param name="entity">The entity to check against.</param>
+        /// <param name="state">The current global state.</param>
+        /// <returns>True if the action can be performed, false otherwise.</returns>
         public abstract bool CanPerformAction(EntityState entity, GlobalUIState state);
 
+        /// <summary>
+        /// Performs the action.
+        /// </summary>
+        /// <param name="entity">The entity to perform the action on.</param>
+        /// <param name="state">The current global state</param>
+        /// <remarks>
+        /// This method should only be called if <see cref="CanPerformAction"/> returns true for the given entity and state.
+        /// </remarks>
         public abstract void PerformAction(EntityState entity, GlobalUIState state);
     }
 
@@ -22,6 +39,12 @@ namespace Pulsar4X.Client
             new PinCameraAction(),
             new RenameEntityAction(),
             new OpenFireControlAction(),
+            new OpenCargoTransferAction(),
+            new GotoSystemAction(),
+            new OpenWarpToAction(),
+            new ChangeOrbitAction(),
+            new OpenNavigationAction(),
+            new OpenOrdersAction(),
         ];
 
         GlobalUIState _state = state;
@@ -46,6 +69,7 @@ namespace Pulsar4X.Client
 
             ImGui.BeginGroup();
 
+            // Creates the context buttons for the current entity.
             foreach (var action in _actions)
             {
                 if (action.CanPerformAction(_entityState, _state))
@@ -56,30 +80,6 @@ namespace Pulsar4X.Client
                     }
                 }
             }
-
-            void ContextButton(Type T)
-            {
-                //Creates a context button if it is valid
-                if (EntityUIWindows.CheckIfCanOpenWindow(T, _entityState, _state))
-                {
-                    if (ImGui.SmallButton(GlobalUIState.NamesForMenus[T]))
-                    {
-                        EntityUIWindows.OpenUIWindow(T, _entityState, _state, true, true);
-                    }
-                }
-            }
-
-            //Creates all the context buttons
-            // ContextButton(typeof(SelectPrimaryBlankMenuHelper));
-            // ContextButton(typeof(PinCameraBlankMenuHelper));
-            // ContextButton(typeof(RenameWindow));
-            // ContextButton(typeof(FireControl));
-            ContextButton(typeof(CreateTransferWindow));
-            ContextButton(typeof(GotoSystemBlankMenuHelper));
-            ContextButton(typeof(WarpOrderWindow));
-            ContextButton(typeof(ChangeCurrentOrbitWindow));
-            ContextButton(typeof(NavWindow));
-            ContextButton(typeof(OrdersListWindow));
             ImGui.EndGroup();
         }
     }
@@ -88,7 +88,9 @@ namespace Pulsar4X.Client
 
     public class SelectPrimaryEntityAction : EntityContextAction
     {
-        public override string Name => "Select Primary";
+        public override string Name => "Select as primary";
+
+        public override bool CanPerformAction(EntityState entity, GlobalUIState state) => true;
 
         public override void PerformAction(EntityState entity, GlobalUIState state)
         {
@@ -98,7 +100,6 @@ namespace Pulsar4X.Client
             }
         }
 
-        public override bool CanPerformAction(EntityState entity, GlobalUIState state) => true;
     }
 
     public class PinCameraAction : EntityContextAction
@@ -142,6 +143,112 @@ namespace Pulsar4X.Client
             var instance = FireControl.GetInstance(entity);
             instance.ToggleActive();
             state.ActiveWindow = instance;
+        }
+    }
+
+    public class OpenCargoTransferAction : EntityContextAction
+    {
+        public override string Name => "Cargo";
+
+        public override bool CanPerformAction(EntityState entity, GlobalUIState state)
+        {
+            return EntityUtility.ResolveEntity(entity, state)?.HasView<CargoStorageView>() ?? false;
+        }
+
+        public override void PerformAction(EntityState entity, GlobalUIState state)
+        {
+            if (entity.StarSystemId != null)
+            {
+                var instance = CreateTransferWindow.GetInstance();
+                instance.SetLeft(entity.Id, entity.StarSystemId);
+                instance.ToggleActive();
+                state.ActiveWindow = instance;
+            }
+        }
+    }
+
+    public class GotoSystemAction : EntityContextAction
+    {
+        public override string Name => "Go to system";
+
+        public override bool CanPerformAction(EntityState entity, GlobalUIState state)
+        {
+            return EntityUtility.ResolveEntity(entity, state)?.GetView<GravSurveyView>()?.JumpPointToSystemId != null;
+        }
+
+        public override void PerformAction(EntityState entity, GlobalUIState state)
+        {
+            var destId = EntityUtility.ResolveEntity(entity, state)?.GetView<GravSurveyView>()?.JumpPointToSystemId;
+            if (destId is not null)
+            {
+                state.SetActiveSystem(destId);
+            }
+        }
+    }
+
+    public class OpenWarpToAction : EntityContextAction
+    {
+        public override string Name => "Warp to new orbit";
+
+        public override bool CanPerformAction(EntityState entity, GlobalUIState state)
+        {
+            return EntityUtility.ResolveEntity(entity, state)?.HasView<WarpAbilityView>() ?? false;
+        }
+
+        public override void PerformAction(EntityState entity, GlobalUIState state)
+        {
+            var instance = WarpOrderWindow.GetInstance(entity);
+            instance.ToggleActive();
+            state.ActiveWindow = instance;
+        }
+    }
+
+    public class ChangeOrbitAction : EntityContextAction
+    {
+        public override string Name => "Change current orbit";
+
+        public override bool CanPerformAction(EntityState entity, GlobalUIState state)
+        {
+            return EntityUtility.ResolveEntity(entity, state)?.HasView<ThrustView>() ?? false;
+        }
+
+        public override void PerformAction(EntityState entity, GlobalUIState state)
+        {
+            var instance = ChangeCurrentOrbitWindow.GetInstance(entity);
+            instance.SetActive(true);
+            state.ActiveWindow = instance;
+        }
+    }
+
+    public class OpenNavigationAction : EntityContextAction
+    {
+        public override string Name => "Nav window";
+
+        public override bool CanPerformAction(EntityState entity, GlobalUIState state)
+        {
+            return EntityUtility.ResolveEntity(entity, state)?.HasView<ThrustView>() ?? false;
+        }
+
+        public override void PerformAction(EntityState entity, GlobalUIState state)
+        {
+            var instance = NavWindow.GetInstance(entity);
+            instance.ToggleActive();
+            state.ActiveWindow = instance;
+        }
+    }
+
+    public class OpenOrdersAction : EntityContextAction
+    {
+        public override string Name => "Orders window";
+
+        public override bool CanPerformAction(EntityState entity, GlobalUIState state)
+        {
+            return EntityUtility.ResolveEntity(entity, state)?.HasView<OrdersView>() ?? false;
+        }
+
+        public override void PerformAction(EntityState entity, GlobalUIState state)
+        {
+            state.WindowManager.ActivateOrderListWindow(entity);
         }
     }
 
